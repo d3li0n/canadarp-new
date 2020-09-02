47ad5462c75c4ec568eac6116a56f328213d8f6e
Dmytro Zhuravel <dmitrozhyravel@gmail.com>
2020-09-02 15:02:33 -0700

    first commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#scroll_1, #scroll_2").mPageScroll2id();
+	$('#cd-dropdown').dropdown();
+	$('select').selecter();
+});
+document.addEventListener('DOMContentLoaded', function(){
+    Typed.new('.typed-text', {
+    strings: ["Окунись в мир новых возможностей <br> вместе с нами"],
+    typeSpeed: 0
+  });
+});
